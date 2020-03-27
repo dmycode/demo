6143aff91bc434fb5c86d26e979b3f6e2a98bf40
dengyong <372646753@qq.com>
2020-03-27 20:31:53 +0800

    数据库配置

---
+++ src/main/java/com/example/demo/config/DataSourceConfiguration.java
@@ -0,0 +1,26 @@
+
+
+    /**  
+    * @Title: DataSourceConfiguration.java
+    * @Package com.example.demo.config
+    * @Description: TODO(用一句话描述该文件做什么)
+    * @author dy
+    * @date 2020年3月27日
+    * @version V1.0  
+    */
+
+    
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+    * @ClassName: DataSourceConfiguration
+    * @Description: TODO(这里用一句话描述这个类的作用)
+    * @author dy
+    * @date 2020年3月27日
+    */
+@Configuration
+public class DataSourceConfiguration {
+
+}

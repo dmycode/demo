84ccba438940bbb4c17605d540c38865c9dd863c
dymcode <372646753@qq.com>
2020-03-30 15:01:28 +0800

    增加注解

--- src/main/java/com/example/demo/service/impl/DemoServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/DemoServiceImpl.java
@@ -24,30 +24,30 @@ import com.example.demo.service.DemoService;
 @Service
 public class DemoServiceImpl implements DemoService {
 
-	/**
-	* @Title:judageScore
-	* @Description:TODO(这里用一句话描述这个方法的作用)
-	* @param score
-	* @return
-	* @throws Exception
-	* @see com.example.demo.service.DemoService#judageScore(java.lang.Integer)
-	*/
+    /**
+    * @Title:judageScore
+    * @Description:TODO(这里用一句话描述这个方法的作用)
+    * @param score
+    * @return
+    * @throws Exception
+    * @see com.example.demo.service.DemoService#judageScore(java.lang.Integer)
+    */
 
-	@Override
-	public String judageScore(Integer score) throws Exception {
-		Assert.notNull(score, "成绩不能为空");
-		String result = "成绩无效";//输入错误提示
-		//（定义一个String的result的变量，结果赋值给result）
-		if (score >= 90) {
-			result = "A";     //输入数据满足条件时，终止程序并输出结果
-		} else if (score >= 80) {//不瞒足条件时，继续向下运行程序
-			result = "B";       //以此类推
-		} else if (score >= 60) {
-			result = "C";
-		} else if (score >= 0) {
-			result = "D";
-		}
-		return result;  // 终止程序。
-	}
+    @Override
+    public String judageScore(Integer score) throws Exception {
+        Assert.notNull(score, "成绩不能为空"); // 校验输入的分数是否为空，如果为空抛出非法参数异常（IllegalArgumentException），提示信息："成绩不能为空"
+        String result = "成绩无效";// 输入错误提示
+        // （定义一个String的result的变量，结果赋值给result），返回结果提前初始化。
+        if (score >= 90) {
+            result = "A"; // 输入数据满足条件时，终止程序并输出结果
+        } else if (score >= 80) {// 不瞒足条件时，继续向下运行程序
+            result = "B"; // 以此类推
+        } else if (score >= 60) {
+            result = "C";
+        } else if (score >= 0) {
+            result = "D";
+        }
+        return result; // 终止程序。
+    }
 
 }
